2452498f14763e293391cd9f453098d5d8e5ebb2
sam-fda <deniz@spaceandmatter.nl>
2026-09-09 15:46:41 +0200

    Update zeist.js

--- zeist.js
+++ zeist.js
@@ -23,7 +23,7 @@ window.TOUR = {
         "fovMin": 25,
         "fovMax": 90,
         "yawLimited": true,
-        "yawRange": 75,
+        "yawRange": 120,
         "pitchLimited": true,
         "pitchUp": 10,
         "pitchDown": 25
